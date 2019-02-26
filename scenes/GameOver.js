fed273e61f26b7e10c7f83948fb67f2fe39b2227
Lorenzo <tognesco@libero.it>
2019-02-26 11:38:26 +0100

    Bug Fix: Avoid having two calls to newAnimal()

--- scenes/GameOver.js
+++ scenes/GameOver.js
@@ -16,7 +16,11 @@ class GameOver extends Phaser.Scene {
   create(data) {
     var background = this.add.image(0, 0, "backgroundGameOver").setOrigin(0, 0);
     this.add.bitmapText(145, 240, "fontGameOver", `Game Over`, 80);
-    this.add.bitmapText(159, 300, "fontGameOver", `Score: ${data - 10}`, 80);
+    if (data - 10 >= 100) {
+      this.add.bitmapText(145, 300, "fontGameOver", `Score: ${data - 10}`, 80);
+    } else {
+      this.add.bitmapText(162, 300, "fontGameOver", `Score: ${data - 10}`, 80);
+    }
   }
 }
 
